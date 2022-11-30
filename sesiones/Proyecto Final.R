b56ab8117e17a52b4eaca2ac22c92e5b846a7a47
Paulojojo <89060722+Paulojojo@users.noreply.github.com>
2022-11-29 23:00:34 -0500

    Anotaciones

---
+++ sesiones/Proyecto Final.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+url <- "https://www.datosabiertos.gob.pe/node/3008/download"
+
+# posible base a trabajar
+
+data <- read.csv(url)
+names(data)
+
+data |> 
+  group_by(Diagnostico) |>
+  summarise(sum = sum(Atendidos)) |> 
+  arrange(-sum)
+  
+
